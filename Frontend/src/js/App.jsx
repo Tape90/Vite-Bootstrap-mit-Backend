be3227423f0354c01643903b6fea5964b4fcf9a7
Tape90 <119531778+Tape90@users.noreply.github.com>
2023-04-17 10:22:16 +0200

    first commit

---
+++ Frontend/src/js/App.jsx
@@ -0,0 +1,20 @@
+//Components
+
+
+//CSS
+import '../scss/App.css'
+import '../scss/index.css'
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+export default App
